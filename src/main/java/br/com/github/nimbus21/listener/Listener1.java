c1d34d5a3e24ecb41f13b00d92086e2610cdc1bd
Nimbus21 <thandy_97@hotmail.com>
2020-09-07 13:35:56 -0300

    UTF-8 corrigido

--- src/main/java/br/com/github/nimbus21/listener/Listener1.java
+++ src/main/java/br/com/github/nimbus21/listener/Listener1.java
@@ -9,39 +9,32 @@ public class Listener1 implements MessageCreateListener {
 	int contadorHusbando = 0;
 
 	public Listener1() {
-
 	}
 
+	@Override
 	public void onMessageCreate(MessageCreateEvent event) {
-
 		Message message = event.getMessage();
 
 		if (message.getContent().equalsIgnoreCase("$ha")) {
-			contadorHusbando++;
-			event.getChannel().sendMessage("Tá rodando aí mano? Contador atualmente em... " + contadorHusbando);
+			event.getChannel().sendMessage("TÃ¡ rodando aÃ­ mano? Contador atualmente em..." + contadorHusbando);
 		}
 
-		if (message.getContent().equalsIgnoreCase("$wa") || message.getContent().equalsIgnoreCase("$ma")) {
-			event.getChannel().sendMessage("Tá rodando aí mano?");
+		if (message.getContent().equalsIgnoreCase("$wa")) {
+			event.getChannel().sendMessage("TÃ¡ rodando aÃ­ mano?");
 		}
 
 		if (message.getContent().equalsIgnoreCase("r!zera")) {
-			contadorHusbando = 0;
-			event.getChannel().sendMessage("Blz, tá zerado essa porra");
+			event.getChannel().sendMessage("Blz, tÃ¡ zerado essa porra");
 		}
-
 		if (message.getContent().equalsIgnoreCase("r!rita")) {
-			event.getChannel().sendMessage("Quem apagar o público tá fudido!");
+			event.getChannel().sendMessage("Quem apagar o pÃºblico tÃ¡ fudido");
 		}
-
 		if (message.getContent().equalsIgnoreCase("r!publico")) {
-			event.getChannel().sendMessage("O público tá vivo");
+			event.getChannel().sendMessage("O pÃºblico tÃ¡ vivo");
 		}
-
 		if (message.getContent().equalsIgnoreCase("r!salva")) {
 			event.getChannel().sendMessage("Vou tentar...");
 		}
-
 	}
 
 }
