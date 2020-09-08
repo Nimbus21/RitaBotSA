6394640ac343264318eca37796f7bc4e4e267aa8
Nimbus21 <thandy_97@hotmail.com>
2020-09-08 03:38:48 -0300

    Contador com start e stop no bd

--- src/main/java/br/com/github/nimbus21/Main.java
+++ src/main/java/br/com/github/nimbus21/Main.java
@@ -3,7 +3,7 @@ package br.com.github.nimbus21;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
-import br.com.github.nimbus21.listener.Listener1;
+import br.com.github.nimbus21.listener.Listener2;
 
 public class Main {
 
@@ -12,24 +12,14 @@ public class Main {
 		
 		DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 		
-		Listener1 listener1 = new Listener1();
+		//Listener1 listener1 = new Listener1();
 		
-		api.addListener(listener1);
+		//api.addListener(listener1);
 		
-//		Listener2 listener2 = new Listener2();
-//		
-//		api.addListener(listener2);
+		Listener2 listener2 = new Listener2();
 		
-		//List<String> comandosCasar = new ArrayList<String>();
+		api.addListener(listener2);
 		
-		//Adicionador eu = new Adicionador();
-		
-		//Adicionador.adicionaAK(comandosCasar);
-		
-		//System.out.println(comandosCasar.contains("our house"));
-		
-		//System.out.println(listener1.getComandosCasar().get(0));
-		//System.out.println();
 	}
 
 }
--- src/main/java/br/com/github/nimbus21/listener/Listener2.java
+++ src/main/java/br/com/github/nimbus21/listener/Listener2.java
@@ -1,106 +1,70 @@
 package br.com.github.nimbus21.listener;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.Optional;
 
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAuthor;
-import org.javacord.api.entity.message.MessageSet;
+import org.javacord.api.entity.message.embed.Embed;
+import org.javacord.api.entity.message.embed.EmbedImage;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
 public class Listener2 implements MessageCreateListener {
 
-	int contadorHusbando = 0;
+	private int contadorRolls = 0;
 
-	List<String> comandosCasar = new ArrayList<String>();
-
-	public List<String> getComandosCasar() {
-		return comandosCasar;
-	}
-
-	public Listener2() {
-		comandosCasar.add("$ha");
-		comandosCasar.add("$wa");
-		comandosCasar.add("$ma");
-		comandosCasar.add("$hg");
-		comandosCasar.add("$wg");
-		comandosCasar.add("$HA");
-		comandosCasar.add("$WA");
-		comandosCasar.add("$MA");
-		comandosCasar.add("$HG");
-		comandosCasar.add("$WG");
-	}
+	private boolean contaBot = false;
 
 	@Override
 	public void onMessageCreate(MessageCreateEvent event) {
 		MessageAuthor messageAuthor = event.getMessageAuthor();
+		Message message = event.getMessage();
 
-		CompletableFuture<MessageSet> messageSet = null;
-		MessageSet aqui = null;
-
-		if (messageAuthor.getName().equalsIgnoreCase("Mudamaid 23")) {
-
-			messageSet = event.getMessage().getMessagesBefore(1);
-		}
-
-//		if (messageAuthor.getName().equalsIgnoreCase("Nimbus21")) {
-//			System.out.println("Nimbus falou");
-//		}
-//		
-//		if (messageAuthor.getName().equalsIgnoreCase("Mudamaid 23")) {
-//			System.out.println("Bot falou");
-//		}
-
-		try {
-			if (messageSet == null) {
-
-			} else {
-				aqui = messageSet.get();
-			}
-
-		} catch (InterruptedException e) {
-			System.out.println("Não consegui");
-		} catch (ExecutionException e) {
-			System.out.println("Não consegui");
-		}
-
-		// System.out.println("Aqui ta vazio?" + aqui.isEmpty());
-		// System.out.println("Aqui2 é oq?" + aqui2);
-
-		if (aqui == null) {
-
-		} else {
+		if (contaBot == true && messageAuthor.getName().equalsIgnoreCase("Mudamaid 23")) {
 
 			try {
-				Message messaga = aqui.first();
+				Embed embed = message.getEmbeds().get(0);
+				Optional<EmbedImage> imagemEmbedada = embed.getImage();
+				contadorRolls++;
+				event.getChannel().sendMessage("Mudamaid 23 mandou waifu. Contador atualmente em " + contadorRolls);
+			} catch (Exception e) {}
 
-				if (comandosCasar.contains(messaga.getContent())) {
-					contadorHusbando++;
-
-					String nome = messaga.getAuthor().getName();
-
-					System.out.println(nome);
-					System.out.println(messaga.getContent());
-					event.getChannel().sendMessage("Mudamaid 23 mandou waifu");
-					event.getChannel().sendMessage("Contador de waifus enviadas na rodada: " + contadorHusbando);
-				}
-
-			} catch (Exception e) {
-				System.out.println("algo diferente de mensagem");
-			}
 		}
 
-		// System.out.println(message.isDone());
+		if (message.getContent().equalsIgnoreCase("r!zera")) {
+			contadorRolls = 0;
+			event.getChannel().sendMessage("Blz, tá zerado essa porra");
+		}
+		if (message.getContent().equalsIgnoreCase("r!rita")) {
+			event.getChannel().sendMessage("Quem apagar o público tá fudido");
+		}
+		if (message.getContent().equalsIgnoreCase("r!publico")) {
+			event.getChannel().sendMessage("O público tá vivo");
+		}
+		if (message.getContent().equalsIgnoreCase("r!salva")) {
+			event.getChannel().sendMessage("Vou tentar...");
+		}
+		if (message.getContent().equalsIgnoreCase("r!contador")) {
+			event.getChannel().sendMessage("Contador em " + contadorRolls);
+		}
 
-		// Message messageRaiz = message.
+		if (message.getContent().equalsIgnoreCase("r!start")) {
+			contaBot = true;
+			event.getChannel().sendMessage("Opa, estou de olho em você!");
+		}
 
-		// System.out.println("mostrar o aqui " + aqui.toString());
-		// System.out.println("Mostrar o completable " + message.toString());
-
-		// System.out.println("mostrar a raiz " + messageRaiz.getContent());
+		if (message.getContent().equalsIgnoreCase("r!stop")) {
+			contaBot = false;
+			event.getChannel().sendMessage("Vou ver como está o público e já volto");
+		}
+		
+		if (message.getContent().equalsIgnoreCase("r!status")) {
+			if (contaBot == true) {
+				event.getChannel().sendMessage("Eu estou monitorando vocês! Mwahahahah");
+			} else {
+				event.getChannel().sendMessage("Eu estou vendo as tabelas do meu banco de dados agora, não tenho tempo pra vocês");
+			}
+		}
 	}
 
 }
